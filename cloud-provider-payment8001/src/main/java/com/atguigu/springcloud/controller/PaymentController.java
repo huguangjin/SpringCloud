0089aa7ec732b6de41ec723a925ddc9c72d0a74f
huguangjin <h1511951618@163.com>
2020-08-27 15:42:04 +0800

    配置SpringCloud Sleuth分布式请求链路跟踪

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -75,4 +75,9 @@ public class PaymentController {
         }
         return serverPort;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+        return "paymentZipkin...";
+    }
 }
